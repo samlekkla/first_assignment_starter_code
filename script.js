6527f7e2d0742f12091b5f9fb92b32db9a5129a4
Helena <helena@klarr.se>
2024-03-20 11:00:42 +0100

    added comments

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const taskManager = {
 function askUserName() {
   // add logic here to as the user to enter their name
   alert(
-    "This is the alert() function which dispays some text for the user.\nUnlike the prompt(), the alert() does not take any input.\nUse it only to display messages for the user."
+    "This is the alert() function which dispays some text for the user.\n" +
+      "Unlike the prompt(), the alert() does not take any input.\nUse it only to display messages for the user."
   );
   menu();
 }
@@ -18,9 +19,13 @@ function askUserName() {
 function menu() {
   const choice = parseInt(
     prompt(
-      "Hello, this is a the prompt function that displays an input and some text.\nWe use parseInt() to make sure that we can take an int as a input from the user.\nBy default the prompt() function takes all input as strings."
+      "Hello, this is a the prompt function that displays an input and some text.\n" +
+        "We use parseInt() to make sure that we can take an int as a input from the user.\n" +
+        "By default the prompt() function takes all input as strings."
     )
   );
+  // add some more3 logic here
+  // choose between if/else or switch
 }
 
 // starts with the function asUserName() which triggers the menu() function
